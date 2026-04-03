c553ef84fc2dac035e0c4934fdffb6600d2b8caf
suraj <job.surajmahamuni@gmail.com>
2026-04-02 21:37:22 -0700

    first

---
+++ strings/reverse order of words.py
@@ -0,0 +1,12 @@
+## User input
+str=input("Enter the string")
+
+## split the string
+l=str.split()  ## [Python,is,easy]
+
+## reverse the list
+rev_l=l[::-1]  ## [easy,is,Python]
+
+## join the reversed list
+output=" ".join(rev_l)
+print(output)
