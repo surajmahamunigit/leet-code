d76e9fbf35c884ef5a24ea6a10fd66b4a031fd25
suraj <job.surajmahamuni@gmail.com>
2026-04-05 15:38:47 -0700

    first commit

---
+++ dictonary/Anagram strings.py
@@ -0,0 +1,20 @@
+#write a program to compare two strings are anagram are not
+
+#two strings are called anagram of both contain same letters, order of letters doesnt matter
+#input1:"lazy"
+#input2:"zaly"  #anagram strings
+
+#user input
+s1="lazy"
+s2="zaly"
+
+#core logic
+
+#sort both the string before you compare them
+#because if you just compare s1==s2, it will also consider the position of letters
+
+if sorted(s1)==sorted(s2):
+    print("s1 and s2 are anagram strings")
+
+else:
+    print("s1 and s2 are not anagram strings")
