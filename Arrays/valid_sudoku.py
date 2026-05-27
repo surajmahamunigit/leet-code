56b28fd51910f82c108ba10c5f51a86f9573a04f
suraj <job.surajmahamuni@gmail.com>
2026-05-26 23:52:43 -0700

    feat : function to find if sudoku is valid or not

---
+++ Arrays/valid_sudoku.py
@@ -0,0 +1,40 @@
+"""
+You are given a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+
+Each row must contain the digits 1-9 without duplicates.
+Each column must contain the digits 1-9 without duplicates.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+Return true if the Sudoku board is valid, otherwise return false
+
+Note: A board does not need to be full or be solvable to be valid.
+
+Time:
+Space:
+"""
+import collections
+
+
+class Solution:
+    def isValidSudoku(self, board:list[list[str]]) -> bool:
+
+        columns = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == '.':
+                    continue
+
+                if (board[row][col] in rows[row] or board[row][col] in columns[col] or board[row][col] in squares[(row // 3, col // 3)]):
+                    return False
+
+                columns[col].add(board[row][col])
+                rows[row].add(board[row][col])
+                squares[(row // 3, col // 3)].add(board[row][col])
+
+        return True
+
+board = [["1","2",".",".","3",".",".",".","."],["4",".",".","5",".",".",".",".","."],[".","9","8",".",".",".",".",".","3"],["5",".",".",".","6",".",".",".","4"],[".",".",".","8",".","3",".",".","5"],["7",".",".",".","2",".",".",".","6"],[".",".",".",".",".",".","2",".","."],[".",".",".","4","1","9",".",".","8"],[".",".",".",".","8",".",".","7","9"]]
+result = Solution().isValidSudoku(board)
+print(result)
