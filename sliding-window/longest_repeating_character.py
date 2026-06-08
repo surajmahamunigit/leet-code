8b4b2a70654d05286a54e2b62e326c2ca604dd88
suraj <job.surajmahamuni@gmail.com>
2026-06-07 23:03:43 -0700

    feat : function to find longest repeating character replacement

---
+++ sliding-window/longest_repeating_character.py
@@ -0,0 +1,35 @@
+"""
+You are given a string s consisting of only uppercase english characters and an integer k. You can choose up to k characters of the string and replace them with any other uppercase English character.
+
+After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
+
+Example 1:
+
+Input: s = "XYYX", k = 2
+
+Output: 4
+
+Time:
+Space:
+"""
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        count = {}
+        res = 0
+
+        l = 0
+        maxf = 0
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r-l+1)
+
+        return res
