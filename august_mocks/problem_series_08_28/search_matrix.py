d7723a1e387d92827cb2b58dadd016969f0d283c
suraj <job.surajmahamuni@gmail.com>
2026-08-28 12:27:18 -0700

    feat : function to find target in m*n matrix

---
+++ august_mocks/problem_series_08_28/search_matrix.py
@@ -0,0 +1,49 @@
+# 12.20
+
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        """Find target number in given sorted matrix.
+
+        Args:
+            matrix: given m*n matrix
+            target: target number
+
+        Returns:
+            True if the target number is in matrix, else False
+
+        Time: O(log (m*n)) - m,n = m*n matrix
+        Space: O(1)
+        """
+
+        left = 0
+        right = len(matrix) * len(matrix[0]) - 1
+
+        while left <= right:
+
+            index = (left + right) // 2
+            row = index // len(matrix[0])
+            col = index % len(matrix[0])
+            val = matrix[row][col]
+
+            if val == target:
+                return True
+
+            if val < target:
+                left = index + 1
+            else:
+                right = index - 1
+
+        return False
+
+s = Solution()
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+assert s.searchMatrix(matrix, 3) == True
+assert s.searchMatrix(matrix, 13) == False
+assert s.searchMatrix(matrix, 1) == True
+assert s.searchMatrix(matrix, 60) == True
+assert s.searchMatrix([[1]], 1) == True
+assert s.searchMatrix([[1,3,5,7]], 7) == True
+assert s.searchMatrix([[1],[3],[5],[7]], 5) == True
+print("passed")
+
+# 12.26 -> 6 min to solve
