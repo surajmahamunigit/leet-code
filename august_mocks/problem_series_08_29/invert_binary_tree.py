d27b998ac0a2fa22ffe7a25e0c4a4c1273f15b57
suraj <job.surajmahamuni@gmail.com>
2026-08-30 14:20:15 -0700

    feat : function to invert given binary tree

---
+++ august_mocks/problem_series_08_29/invert_binary_tree.py
@@ -0,0 +1,35 @@
+# 2.15
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        """Invert the given binary tree.
+
+        Args:
+            root: given binary tree
+
+        Returns:
+            inverts the given binary tree
+
+        Time: O(n) - n = total nodes in binary tree
+        Space: O(h) - h = height of the binary tree
+        """
+
+        # base case
+        if not root:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+# 2.19 -> 4 min to solve
