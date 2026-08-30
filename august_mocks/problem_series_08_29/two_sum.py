83a8144b44c4e0aca6c0e81bac428e807d7c5564
suraj <job.surajmahamuni@gmail.com>
2026-08-30 10:35:05 -0700

    feat : function to find 1-indexed position of two numbers that add up to target

---
+++ august_mocks/problem_series_08_29/two_sum.py
@@ -0,0 +1,42 @@
+# 10.28
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        """Find two numbers that add up to target and return their 1-indexed positions.
+
+        Args:
+            numbers: list of sorted numbers
+            target: target sum
+
+        Returns:
+            1-indexed position of two numbers that add up to target
+
+        Time: O(n) -  n = len(numbers)
+        Space: O(1)
+
+        """
+
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            curr_sum = numbers[left] + numbers[right]
+
+            if curr_sum < target:
+                left += 1
+            elif curr_sum > target:
+                right -= 1
+            else:
+                return [left+1, right+1]
+
+        return []
+
+s = Solution()
+assert s.twoSum([2,7,11,15], 9) == [1,2]
+assert s.twoSum([1,2,3,4,4,9,56,90], 8) == [4,5]
+assert s.twoSum([-1,0,2,5,9,12], 7) == [3,4]
+assert s.twoSum([1,2], 3) == [1,2]
+assert s.twoSum([0,0,3,4], 0) == [1,2]
+print("passed")
+
+# 10.34 -> 6 min
