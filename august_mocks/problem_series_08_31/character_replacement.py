014ff076ef2cfe04f792c1290ea0a96735b5a25b
suraj <job.surajmahamuni@gmail.com>
2026-08-31 11:13:11 -0700

    feat : function to length of longest substring with k replacements

---
+++ august_mocks/problem_series_08_31/character_replacement.py
@@ -0,0 +1,42 @@
+# 11.01
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        """Find the length of longest substring with unique character and k replacements.
+
+        Args:
+            s: given string
+            k: number of character replacements allowed
+
+        Returns:
+            length of longest substring with unique character and k replacements
+
+        Time: O(n) - n = len(s)
+        Space: O(1)
+        """
+        longest = 0
+        count = [0] * 26
+        left = 0
+        max_count = 0
+
+        for index in range(len(s)):
+            idx = ord(s[index]) - ord("A")
+            count[idx] += 1
+            max_count = max(max_count, count[idx])
+            curr_len = index - left + 1
+
+            if curr_len - max_count > k:
+                count[ord(s[left]) - ord("A")] -= 1
+                left += 1
+
+            longest = max(longest, index - left + 1)
+
+        return longest
+
+s = Solution()
+assert s.characterReplacement("ABAB", 2) == 4
+assert s.characterReplacement("AABABBA", 1) == 4
+assert s.characterReplacement("", 0) == 0
+assert s.characterReplacement("AAAA", 0) == 4
+assert s.characterReplacement("A", 0) == 1
+print("passed")
