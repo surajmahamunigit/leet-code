985cb1949e864334e6f00eb7765fe780570a879c
suraj <job.surajmahamuni@gmail.com>
2026-09-01 15:03:44 -0700

    feat : function to merge two sorted lists

---
+++ august_mocks/problem_series_09_01/merge_sorted_list.py
@@ -0,0 +1,41 @@
+# 2.57
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """Merge the two sorted lists and return it as a new list.
+
+        Args:
+            l1, l2: given sorted lists
+
+        Returns:
+            merged sorted list
+
+        Time: O(m+n) - m, n = total number of nodes in l1 and l2
+        Space: O(1)
+        """
+
+        dummy = ListNode()
+        curr = dummy
+
+        while l1 or l2:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            if val1 <= val2:
+                curr.next = l1
+                l1.next = l2
+            else:
+                curr.next = l2
+                l2.next = l1
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+
+# 3.03
