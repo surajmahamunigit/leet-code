3555200409c87df4be3148170523702d79c5590d
suraj <job.surajmahamuni@gmail.com>
2026-09-01 15:12:33 -0700

    feat : function to merge two sorted lists

--- august_mocks/problem_series_09_01/merge_sorted_list.py
+++ august_mocks/problem_series_09_01/merge_sorted_list.py
@@ -22,20 +22,20 @@ class Solution:
         dummy = ListNode()
         curr = dummy
 
-        while l1 or l2:
+        while l1 and l2:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
 
             if val1 <= val2:
                 curr.next = l1
-                l1.next = l2
+                l1 = l1.next if l1 else None
             else:
                 curr.next = l2
-                l2.next = l1
+                l2 = l2.next if l2 else None
 
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            curr = curr.next
 
+        curr.next = l1 if l1 else l2
         return dummy.next
 
 # 3.03
