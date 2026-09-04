a85d02268d5ecffab5d841df86e0d5619eaba14d
suraj <job.surajmahamuni@gmail.com>
2026-09-03 19:35:06 -0700

    feat : function to find depth of binary tree

---
+++ september_mocks/daily_mock_09_03/max_depth.py
@@ -0,0 +1,54 @@
+# 7.25
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepthRecursive(self, root: TreeNode) -> int:
+        """Find the maximum depth of the given binary tree.
+
+        Args:
+            root (TreeNode): root node of the given binary tree
+
+        Returns:
+            int: maximum depth of the given binary tree
+
+        Time: O(n) - n = number of nodes in the tree
+        Space: O(h) - h = height of the given tree
+        """
+
+        # base case
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepthRecursive(root.left), self.maxDepthRecursive(root.right))
+
+
+    def maxDepthIterative(self, root: TreeNode) -> int:
+        """Find the maximum depth of the given binary tree.
+
+        Args:
+            root (TreeNode): root node of the given binary tree
+
+        Returns:
+            int: maximum depth of the given binary tree
+
+        Time: O(n) - n = number of nodes in the tree
+        Space: O(n)
+        """
+        max_depth = 0
+
+        stack = [[root, 1]]
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                max_depth = max(max_depth, depth)
+
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return max_depth
