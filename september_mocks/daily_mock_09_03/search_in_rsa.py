06618a32de20d8857dd0154382f5574d2eb4278f
suraj <job.surajmahamuni@gmail.com>
2026-09-03 19:06:59 -0700

    feat : function to find target number in given rsa

---
+++ september_mocks/daily_mock_09_03/search_in_rsa.py
@@ -0,0 +1,56 @@
+# 6.57
+
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        """Returns the index of target number if found, else -1.
+
+        Args:
+            nums (list[int]): rotated sorted array
+            target (int): target number to search for
+
+        Returns:
+            index of target number if found, else -1
+
+        Time: O(log n) - n = len(nums)
+        Space: O(1)
+        """
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            index = (left + right) // 2
+
+            if nums[index] == target:
+                return index
+
+            # left of index is sorted
+            if nums[left] <= nums[index]:
+                if nums[left] <= target < nums[index]:
+                    right = index - 1
+                else:
+                    left = index + 1
+
+            # right of index is sorted
+            else:
+                if nums[index] < target <= nums[right]:
+                    left = index + 1
+                else:
+                    right = index - 1
+
+        return -1
+
+s = Solution()
+
+assert s.search([4,5,6,7,0,1,2], 0) == 4
+assert s.search([4,5,6,7,0,1,2], 3) == -1
+assert s.search([1], 1) == 0
+assert s.search([1], 0) == -1
+assert s.search([5,1,3], 5) == 0
+assert s.search([1,2,3,4,5], 5) == 4
+assert s.search([3,1], 5) == -1
+
+print('passed')
+
+# 7.06 -> 9 min
