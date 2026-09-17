edcd69d6d92f34872f9b97ead8d98b452fc51fc0
suraj <job.surajmahamuni@gmail.com>
2026-09-16 17:31:27 -0700

    feat : function to find if tree is subtree of another tree

---
+++ september_mocks/daily_mocks_09_16/subtree.py
@@ -0,0 +1,44 @@
+# 4.22
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
+        """Find if the given tree is a subtree of another tree.
+
+        Args:
+            root, subRoot (TreeNode): the root of given trees.
+
+        Returns:
+            bool: True if subRoot is a subtree, otherwise False.
+
+        Time: O(m*n) - m, n = number of nodes in root and subRoot
+
+        Space: O(h1 + h1) - combined height of both trees
+        """
+
+        if not subRoot:
+            return True
+
+        if not root:
+            return False
+
+        if self.sameTree(root, subRoot):
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.sameTree(root.right, subRoot)
+
+    def sameTree(self, s, t) -> bool:
+
+        if not s and not t:
+            return True
+
+        if s and t and s.val == t.val:
+            return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
+
+        return False
