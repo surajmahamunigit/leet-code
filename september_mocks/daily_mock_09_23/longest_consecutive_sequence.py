35015e0f0e258db4c1a39c61f5637a33bcea2439
suraj <job.surajmahamuni@gmail.com>
2026-09-23 19:28:54 -0700

    feat : function to find longest consecutive sequence in the given array

--- september_mocks/daily_mock_09_23/longest_consecutive_sequence.py
+++ september_mocks/daily_mock_09_23/longest_consecutive_sequence.py
@@ -18,13 +18,13 @@ class Solution:
         longest = 0
 
         nums_set = set(nums)
-        for index in range(len(nums_set)):
+        for num in nums_set:
 
-            if nums[index] - 1 in nums_set:
+            if num - 1 in nums_set:
                 continue
 
             length = 0
-            while nums[index] + length in nums_set:
+            while num + length in nums_set:
                 length += 1
                 longest = max(longest, length)
 
@@ -34,3 +34,4 @@ s = Solution()
 
 print(s.longest_consecutive(nums = [2,20,4,10,3,4,5]))
 print(s.longest_consecutive(nums = [0,3,2,5,4,6,1,1]))
+print(s.longest_consecutive(nums = [0, 0, -1]))
